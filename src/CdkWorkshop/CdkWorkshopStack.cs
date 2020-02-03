02487e6f066ae8b26f0f55cfe07096bc11ef9930
Surajit Bera <surajit@dharmah.in>
2020-02-03 15:59:11 +0530

    UPDATE

---
+++ src/CdkWorkshop/CdkWorkshopStack.cs
@@ -0,0 +1,14 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.S3;
+namespace CdkWorkshop 
+ { 
+public class CdkWorkshopStack : Stack  
+ { 
+internal CdkWorkshopStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props) 
+ { 
+//****************Code************* 
+new Bucket(this, "NewBucket");
+//****************End Code************* 
+ } 
+ } 
+ } 
---
+++ src/CdkWorkshop/Program.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+namespace CdkWorkshop
+{ 
+sealed class Program 
+{ 
+public static void Main(string[] args) 
+{ 
+var app = new App(); 
+new CdkWorkshopStack(app, "Stack-aws-s3");
+app.Synth();
+ } 
+ } 
+ } 
