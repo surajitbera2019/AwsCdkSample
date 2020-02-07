ab0d66b44120ec4d7580536169c4f074f30501c5
Surajit Bera <surajit@dharmah.in>
2020-02-07 20:27:33 +0530

    update

--- src/CdkWorkshop/Helper.cs
+++ src/CdkWorkshop/Helper.cs
@@ -3,9 +3,9 @@ namespace CdkWorkshop
 {
   public static class Helper
     {
-        public static  Amazon.CDK.Environment makeEnv(string account=null, string region=null)
+        public static  Environment makeEnv(string account=null, string region=null)
         {
-            return new Amazon.CDK.Environment
+            return new Environment
             {
                 Account = account ?? System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
                 Region = region ?? System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
