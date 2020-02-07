e45544844f7e868b6cab1ca71d34ad426d2b0587
Surajit Bera <surajit@dharmah.in>
2020-02-07 20:26:28 +0530

    code update for env

---
+++ src/CdkWorkshop/Helper.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CDK;
+namespace CdkWorkshop
+{
+  public static class Helper
+    {
+        public static  Amazon.CDK.Environment makeEnv(string account=null, string region=null)
+        {
+            return new Amazon.CDK.Environment
+            {
+                Account = account ?? System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+                Region = region ?? System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
+            };
+        }
+    }
+}
--- src/CdkWorkshop/Program.cs
+++ src/CdkWorkshop/Program.cs
@@ -1,13 +1,16 @@
 using Amazon.CDK;
 namespace CdkWorkshop
-{ 
-sealed class Program 
-{ 
-public static void Main(string[] args) 
-{ 
-var app = new App(); 
-new CdkWorkshopStack(app, "Stack-aws-s3");
-app.Synth();
- } 
- } 
- } 
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var envAny = Helper.makeEnv(account: "", region: "");
+
+            var app = new App();
+            new CdkWorkshopStack(app, "Stack-S3", new StackProps { Env = envAny });
+            app.Synth();
+        }
+    }
+}
